4269045954daeb2c875e35400dc255a14dcf2c39
yangar <supergaillard@yahoo.fr>
2018-03-02 11:46:28 +0700

    Add project files.

---
+++ Api.Forex.Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Api.Forex.Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
